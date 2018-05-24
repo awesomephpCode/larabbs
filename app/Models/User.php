91d9a0acc91004fda6aa80302961b78caae1e0f3
1785301169@qq.com <1785301169@qq.com>
2018-05-24 17:41:32 +0800

    作者可以删除话题

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,14 +7,19 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 
 class User extends Authenticatable
 {
-  use Notifiable;
+    use Notifiable;
 
-  protected $table = 'users';
-  protected $fillable = ['name', 'email', 'password','introduction','avatar'];
-  protected $hidden = ['password', 'remember_token',];
+    protected $table = 'users';
+    protected $fillable = ['name', 'email', 'password','introduction','avatar'];
+    protected $hidden = ['password', 'remember_token',];
 
-  public function topics()
-  {
-    return $this->hasMany(Topic::class);
-  }
+    public function topics()
+    {
+       return $this->hasMany(Topic::class);
+    }
+
+    public function isAuthorOf($model)
+    {
+        return $this->id == $model->user_id;
+    }
 }
